2ace523aedac772eb83638687cddf7af3205a53a
vacantClaudia <vacantclaudia88@gmail.com>
2021-05-04 11:08:48 +0200

    poudlock mode organisé

--- app.js
+++ app.js
@@ -10,8 +10,23 @@ let app = {
     ],
     // Méthode appelée au chargement de la page
     init: function() {
-     
-    }
+       let form= document.querySelector('form');
+       form.addEventListener('submit', app.handleValue);
+    },
+    handleValue: function(evt){
+       evt.preventDefault();
+       const value= document.querySelector('.name').value;
+       if (value===""){
+           alert('Ca ne fonctionne qu\'avec un nom, pardi!')
+       }else if(value.length===8){
+           alert('maison MAXOMUS')
+       }else if (value.startsWith('L') || value.endsWith('x')){
+           alert('maison LUSTRIX') 
+       }else if (value.length % 5 == 0 || value.length % 3 == 0){
+           alert('maison ANTHORVUS')
+       }else{ alert ('maison DARIOPTERA')}
+    },
+    
 }
 
 // Quand la page est entièrement chargée, on exécute la méthode init située dans l'object app.
